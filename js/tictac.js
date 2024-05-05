2ff15ab291683865c51b0a7603b61e832348336a
muka181 <mukamag78@gmail.com>
2024-05-05 15:55:18 +0300

    second commit

---
+++ js/tictac.js
@@ -0,0 +1,14 @@
+let x = document.querySelector("btn1")
+let o = document.querySelector("btn2")
+
+let area1 = document.querySelector(".area1");
+let area2 = document.querySelector(".area2");
+let area3 = document.querySelector(".area3");
+let area4 = document.querySelector(".area4");
+let area5 = document.querySelector(".area5");
+let area6 = document.querySelector(".area6");
+let area7 = document.querySelector(".area7");
+let area8 = document.querySelector(".area8");
+let area9 = document.querySelector(".area9");
+
+
